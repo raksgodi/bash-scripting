2b5926e8b8fa7b6c323543d8c74b7dda699f89ba
raksgodi <drrakeshkumargodi@gmail.com>
2022-09-07 11:09:03 +0530

    variables

--- var.sh
+++ var.sh
@@ -5,5 +5,5 @@
 # Syntax is 
 # echo -e "\e[32m TextOfYourChoice  \e[0m"
 
-echo -e "\e[33m DevOps has a very great future \e[0m"
-echo -e "\e[43;37m DevOps has a very great future \e[0m"
+echo -e "\e[32m DevOps has a very great future \e[0m"
+echo -e "\e[43;34m DevOps has a very great future \e[0m"
