682158cef086a4f59d4f64653e34c6322b061d1b
raksgodi <drrakeshkumargodi@gmail.com>
2022-09-07 11:07:43 +0530

    colors

--- var.sh
+++ var.sh
@@ -1,8 +1,9 @@
 #!/bin/bash 
 
-echo line1
-echo line2   
+#Syntax : echo -e  "\e[ColorCodem Your Tex \e[0m"
 
+# Syntax is 
+# echo -e "\e[32m TextOfYourChoice  \e[0m"
 
-echo -e "line12\nline13"
-echo $0
+echo -e "\e[33m DevOps has a very great future \e[0m"
+echo -e "\e[43;37m DevOps has a very great future \e[0m"
