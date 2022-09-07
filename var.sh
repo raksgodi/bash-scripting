e69ca22ba0797243dff6944e9629b601b0ff7e23
raksgodi <drrakeshkumargodi@gmail.com>
2022-09-07 11:24:09 +0530

    variables

--- var.sh
+++ var.sh
@@ -1,9 +1,12 @@
-#!/bin/bash 
+#!/bin/bash  
 
-#Syntax : echo -e  "\e[ColorCodem Your Tex \e[0m"
+a=10 
+# a is 10 and is integer 
 
-# Syntax is 
-# echo -e "\e[32m TextOfYourChoice  \e[0m"
+b=abc 
+# abc is a string  
 
-echo -e "\e[32m DevOps has a very great future \e[0m"
-echo -e "\e[43;34m DevOps has a very great future \e[0m"
+d=150
+
+DATE_COMMAND=$(date +%F)
+LOGGEDIN_USERS=$(who | wc -l)
